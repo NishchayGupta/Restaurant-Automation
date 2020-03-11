b1dafd03a4a27d2b095d378ee2eabce4a4e91480
akanshnair94 <akansh.nair@yahoo.com>
2020-03-11 14:46:40 -0400

    added the progress on menu screen

--- app/src/main/java/com/hexamind/uniquorestaurant/Customer/MenuCardFragment.java
+++ app/src/main/java/com/hexamind/uniquorestaurant/Customer/MenuCardFragment.java
@@ -14,6 +14,7 @@ import retrofit2.Response;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.hexamind.uniquorestaurant.Data.FoodItem;
@@ -31,30 +32,44 @@ public class MenuCardFragment extends Fragment {
     private View root;
     private MenuCardAdapter adapter;
     private ArrayList<FoodItems> foodItemList = new ArrayList<>();
+    private ProgressBar progress;
     
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.fragment_menu_card, container, false);
 
+        progress = root.findViewById(R.id.progress);
+        progress.setVisibility(View.VISIBLE);
         ApiService api = RetrofitClient.getApiService();
         Call<List<FoodItems>> call = api.getAllFoodItems();
 
         call.enqueue(new Callback<List<FoodItems>>() {
             @Override
             public void onResponse(Call<List<FoodItems>> call, Response<List<FoodItems>> response) {
-                foodItemList = (ArrayList<FoodItems>) response.body();
+                progress.setVisibility(View.GONE);
 
-                recyclerView = root.findViewById(R.id.recyclerView);
-                recyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
-                recyclerView.setItemAnimator(new DefaultItemAnimator());
-                adapter = new MenuCardAdapter(root.getContext(), foodItemList);
-                recyclerView.setAdapter(adapter);
-            }
+                if (response.code() == 200) {
+                    foodItemList = (ArrayList<FoodItems>) response.body();
+
+                    recyclerView = root.findViewById(R.id.recyclerView);
+                    recyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
+                    recyclerView.setItemAnimator(new DefaultItemAnimator());
+                    adapter = new MenuCardAdapter(root.getContext(), foodItemList);
+                    recyclerView.setAdapter(adapter);
+                } else if (response.code() == 400 || response.code() == 401 || response.code() == 404) {
+                    Toast.makeText(root.getContext(), getString(R.string.four_error_message_string), Toast.LENGTH_SHORT).show();
+                } else if (response.code() == 500) {
+                    Toast.makeText(root.getContext(), getString(R.string.five_error_message_string), Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(root.getContext(), getString(R.string.general_error_message_string), Toast.LENGTH_SHORT).show();
+                }
+             }
 
             @Override
             public void onFailure(Call<List<FoodItems>> call, Throwable t) {
-
+                progress.setVisibility(View.GONE);
+                Toast.makeText(root.getContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
 
